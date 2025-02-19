8e950be84429935d6008df26971e2adefbb26f51
bezmari19 <marina.bezerraf19@gmail.com>
2025-02-19 13:09:51 -0300

    commit feito

--- Atividade5.c
+++ Atividade5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) { 
-  
+  /*Escreva um programa que ajuda os alunos a calcularem as notas desta disciplina. O programa deve perguntar as notas da primeira e da segunda prova, calcular a média aritmética entre elas, e exibir na tela.*/
   printf("Calculadora de Notas: \n");
 
     float notaUm;
