d462117e04cc663d72390a5037464d0ab3dd1fc5
bezmari19 <marina.bezerraf19@gmail.com>
2025-03-12 19:24:33 -0300

    commit feito

---
+++ Atividade21.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+/*Faça um algoritmo que receba um número e mostre uma mensagem caso este número seja maior que 80, menor que 25 ou igual a 40.*/
+
+int main(void) {
+  int valorEscolhido;
+  printf("Digite um valor:\n");
+  scanf("%d", &valorEscolhido);
+
+  if 
+}
