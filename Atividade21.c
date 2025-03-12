453845c77aebc17e0675bf50c6c7e67b463787fd
bezmari19 <marina.bezerraf19@gmail.com>
2025-03-12 19:34:40 -0300

    commit feito

--- Atividade21.c
+++ Atividade21.c
@@ -6,5 +6,10 @@ int main(void) {
   printf("Digite um valor:\n");
   scanf("%d", &valorEscolhido);
 
-  if 
+  if (valorEscolhido > 80 && valorEscolhido < 25 || valorEscolhido == 40) {
+    printf("Olha só o valor é maior que 80, menor que 25 e talvez... seja igual a 40! Muito específico você.");
+  }
+  
+  return 0;
+
 }
