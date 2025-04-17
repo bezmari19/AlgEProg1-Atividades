c28ed69bc07cc22c3bf0cd362e6cf51fbc5b939b
bezmari19 <marina.bezerraf19@gmail.com>
2025-04-17 17:09:49 -0300

    commit atividades RETIRADAS

--- Atividade1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> 
-/*Escreva a frase "Bom dia! Estou aprendendo a programar. Definitivamente não é "muito" difícil*/
-int main(void) {
-  
-  printf("Bom dia! Estou aprendendo a programar. Definitivamente não é \"muito\" difícil");
-  
-  return 0;
-}
